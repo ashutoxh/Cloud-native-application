65b4887fa3d7142f729470edc9d979ba81049ab0
Ashutosh Singh <singh.ashuto@northeastern.edu>
2024-01-26 20:47:29 -0500

    init

---
+++ src/main/java/com/csye6225/cloud/CloudNativeApplication.java
@@ -0,0 +1,13 @@
+package com.csye6225.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNativeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csye6225/cloud/CloudNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csye6225.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
